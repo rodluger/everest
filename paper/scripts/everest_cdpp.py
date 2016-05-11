ae12d12ec2f046cc5a2630178079bfa6e2ccaeed
Rodrigo Luger <rodluger@gmail.com>
2016-05-11 14:24:01 -0700

    add crwdsev and satsev metrics

--- paper/scripts/everest_cdpp.py
+++ paper/scripts/everest_cdpp.py
@@ -79,4 +79,4 @@ for campaign in range(7):
         kepmag = data['kepmag'][()]
       
         print("{:>09d} {:>15.3f} {:>15.3f}".format(star, rms_evr, rms_evr_savgol), file = feverest)
-        print("{:>09d} {:>15.3f} {:>15.3f} {:>15.3f} {:>15.3f} {:>15d} {:>15d}".format(star, kepmag, rms_raw, rms_raw_savgol, rms_pht, satsev, crwdsev), file = fraw)
+        print("{:>09d} {:>15.3f} {:>15.3f} {:>15.3f} {:>15.3f} {:>01d} {:>01d}".format(star, kepmag, rms_raw, rms_raw_savgol, rms_pht, satsev, crwdsev), file = fraw)
