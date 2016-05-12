29554aaf0cfbda8fceb67795f8e438b93e042499
Rodrigo Luger <rodluger@gmail.com>
2016-05-12 13:27:41 -0700

    crowding script added

---
+++ scripts/crowding.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+crowding.py
+-----------
+        
+'''
+
+from __future__ import division, print_function, absolute_import, unicode_literals
+import os, sys
+EVEREST_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1, EVEREST_ROOT)
+import everest
+import numpy as np
+import matplotlib.pyplot as pl
+from scipy.ndimage import zoom
+
+def AddApertureContour(ax, nx, ny, aperture):
+  '''
+  
+  '''
+  
+  # Get the indices
+  apidx = np.where(aperture & 1)
+  
+  # Create the array
+  contour = np.zeros((ny,nx))
+  contour[apidx] = 1
+
+  # Add padding around the contour mask so that the edges get drawn
+  contour = np.lib.pad(contour, 1, everest.utils.PadWithZeros)
+
+  # Zoom in to make the contours look vertical/horizontal
+  highres = zoom(contour, 100, order=0, mode='nearest') 
+  extent = np.array([0, nx, 0, ny]) + \
+           np.array([0, -1, 0, -1]) + \
+           np.array([-1, 1, -1, 1])
+
+  # Apply the contours
+  ax.contour(highres, levels=[0.5], extent=extent, 
+             origin='lower', colors='k', linewidths=3)  
+
+  return
+
+def PlotPostageStamp(EPIC, apnum = 15):
+  '''
+  
+  '''
+  
+  # Grab the data
+  data = everest.GetK2Data(EPIC)
+  nearby = data.nearby
+  aperture = data.apertures[apnum]
+  fpix = data.fpix
+  kepmag = data.kepmag
+  _, ny, nx = fpix.shape
+  total_flux = np.log10(np.nansum(fpix, axis = 0))
+
+  # Plot the data and the aperture
+  fig, ax = pl.subplots(1)
+  ax.imshow(total_flux, interpolation = 'nearest', alpha = 0.75)
+  AddApertureContour(ax, nx, ny, aperture)
+
+  # Crop the image
+  ax.set_xlim(-0.7, nx - 0.3)
+  ax.set_ylim(-0.7, ny - 0.3)
+
+  # Overplot nearby sources
+  neighbors = []  
+  def size(k):
+    s = 1000 * 2 ** (kepmag - k)
+    return s
+  for source in nearby:
+    ax.scatter(source.x - source.x0, source.y - source.y0, 
+               s = size(source.kepmag), 
+               c = ['g' if source.epic == EPIC else 'r'],
+               alpha = 0.5,
+               edgecolor = 'k')
+    ax.scatter(source.x - source.x0, source.y - source.y0, 
+               marker = r'$%.1f$' % source.kepmag, color = 'w',
+               s = 500)
+  ax.set_xticklabels([])
+  ax.set_yticklabels([])
+  
+  pl.suptitle('EPIC %d' % EPIC, fontsize = 25, y = 0.98)
+  pl.show()
+  
+  return
+
+PlotPostageStamp(201367065, apnum = 19)
