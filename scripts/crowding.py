9bb561c8e35ad7e591fe106e438b7ff7ac31fe26
Nicholas Saunders <nksaunders1994@gmail.com>
2016-06-12 12:31:40 -0700

    incorperated error funciton

--- scripts/crowding.py
+++ scripts/crowding.py
@@ -16,6 +16,7 @@ import numpy as np
 import matplotlib.pyplot as pl
 from scipy.ndimage import zoom
 from scipy.optimize import fmin
+from scipy.special import erf
 
 def AddApertureContour(ax, nx, ny, aperture):
   '''
@@ -85,13 +86,20 @@ def PlotPostageStamp(EPIC, apnum = 15):
   ax.set_yticklabels([])
   
   pl.suptitle('EPIC %d' % EPIC, fontsize = 25, y = 0.98)
-  pl.show()
+  # pl.show()
   
   return
 
-PlotPostageStamp(201367065, apnum = 15)
+# PlotPostageStamp(201367065, apnum = 15)
+
 data = everest.GetK2Data(201367065)
-total_flux = np.log10(np.nansum(fpix, axis = 0))
+fpix = data.fpix
+_, ny, nx = fpix.shape
+kepmag = data.kepmag
+total_flux = np.transpose(fpix[0])
+errors = np.transpose(data.perr[0])
+
+# print(nx, ny, fpix.shape, total_flux.shape)
 
 def GaussianFit(EPIC):
   data = everest.GetK2Data(EPIC)
@@ -102,32 +110,68 @@ def GaussianFit(EPIC):
   _, ny, nx = fpix.shape
 
 # generate 2 dimensional guassian plot
-def Gaus2D(x, y, xc, yc, a, sigma):
-  return a*np.exp((- (x - xc) ** 2 - (y - yc) ** 2) / (2 * sigma ** 2))
+def Gaus2D(x, y, xc, yc, a, b, sigma):
+  return a*np.exp((- (x - xc) ** 2 - (y - yc) ** 2) / (2 * sigma ** 2)) + b
+
+# generate 2D error function
+def Erf2D(x, y, xc, yc, a, b, sigma):
+  '''
+  A two-dimensional error function (the integral of a Gaussian over a pixel).
+  
+  '''
+  
+  c = np.sqrt(2) * sigma
+  ex = (erf((x + 1 - xc) / c) - erf((x - xc) / c))
+  ey = (erf((y + 1 - yc) / c) - erf((y - yc) / c))
+  return a * ex * ey + b
 
 # define chi squared function
 def ChiSq(params):
-  xc, yc, a, sigma = params
+  xc, yc, a, b, sigma= params
   result = 0
-  for x in range(100):
-    for y in range(100):
-      result += (Gaus2D(x,y,xc,yc,a,sigma) - data[x,y]) ** 2 / errors[x,y] ** 2
+  for x in range(nx):
+    for y in range(ny):
+      if np.isnan(total_flux[x,y]):
+        continue
+      else:
+        result += (Erf2D(x,y,xc,yc,a,b,sigma) - total_flux[x,y]) ** 2 / errors[x,y] ** 2
   return result
 
-# Plot sample chi squared curve
+# plot the data
 fig1 = pl.figure()
+pl.imshow(total_flux)
+pl.title('Data', fontsize = 22)
+pl.colorbar()
+
+# Plot sample chi squared curve
+fig2 = pl.figure()
 sigma_arr = np.linspace(1,10,100)
-pl.plot(sigma_arr, [ChiSq([50., 50., 1., s]) for s in sigma_arr])
+pl.plot(sigma_arr, [ChiSq([9.3, 7.0, 100000., 250., s]) for s in sigma_arr])
+pl.xlim(0, 10)
 pl.title('Chi squared', fontsize = 22)
 
   # Minimize chi squared to find true parameters
-guess = [49., 51., 1.2, 7.]
+guess = [9.3, 7.0, 100000., 250., 1.3]
 res = fmin(ChiSq, guess)
 print(res)
 
 # Generate the model matrix
-xc, yc, a, sigma = res
-model = np.zeros((100, 100))
-for x in range(100):
-  for y in range(100):
-    model[x,y] = Gaus2D(x, y, xc, yc, a, sigma)
+xc, yc, a, b, sigma = res
+model = np.zeros((nx, ny))
+for x in range(nx):
+  for y in range(ny):
+    model[x,y] = Erf2D(x, y, xc, yc, a, b, sigma)
+
+# plot the model
+fig3 = pl.figure()
+pl.imshow(model)
+pl.colorbar()
+pl.title('Model', fontsize = 22)
+
+# Plot the difference
+fig4 = pl.figure()
+pl.imshow(total_flux - model)
+pl.colorbar()
+pl.title('Data - Model', fontsize = 22)
+
+pl.show()
