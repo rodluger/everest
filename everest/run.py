17bf9bd922ed3202f61f2a2608cc3bc1d3c5f337
Rodrigo Luger <rodluger@gmail.com>
2016-06-14 09:18:42 -0700

    move figures to fits folder

--- everest/run.py
+++ everest/run.py
@@ -500,7 +500,7 @@ def _MoveFigures(campaign, ext = 'jpg'):
   
   for i, EPIC in enumerate(stars):
     print("Processing EPIC %d (%d/%d)..." % (EPIC, i + 1, nstars))
-    inpath = os.path.join(EVEREST_DAT, 'output', 'C%02d' % campaign, EPIC, 'default')
+    inpath = os.path.join(EVEREST_DAT, 'output', 'C%02d' % campaign, str(EPIC), 'default')
     outpath = os.path.join(EVEREST_DAT, 'fits', 'c%02d' % campaign, 
                           ('%09d' % EPIC)[:4] + '00000')
     if not os.path.exists(outpath):
