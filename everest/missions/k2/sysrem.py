ba752e1694f0cb185f82b7c8b965f3bad1791110
Rodrigo Luger <rodluger@gmail.com>
2016-12-21 13:14:07 -0200

    sysrem debug

--- everest/missions/k2/sysrem.py
+++ everest/missions/k2/sysrem.py
@@ -323,7 +323,7 @@ def Test():
     # DEBUG
     kernels = None
     
-    new_fluxes = SysRem(time[inds], fluxes[:,inds], errors[:,inds], kernels = kernels, nrec = 2, niter = 50)
+    new_fluxes = SysRem(time[inds], fluxes[:,inds], errors[:,inds], kernels = kernels, nrec = 3, niter = 50)
     
     # Save
     np.savez(outfile, new_fluxes = new_fluxes)
