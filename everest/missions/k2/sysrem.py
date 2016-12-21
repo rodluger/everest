7e88dc86bdafe428c5a0b706b27b3045dc51c5bb
Rodrigo Luger <rodluger@gmail.com>
2016-12-21 13:34:22 -0200

    sysrem debug

--- everest/missions/k2/sysrem.py
+++ everest/missions/k2/sysrem.py
@@ -198,10 +198,7 @@ def Test():
       
       # Align with previous chunk
       if i > 0:
-        
-        import pdb; pdb.set_trace()
-        
-        f += new_fluxes[:,inds[0]-1] - f[:,0].reshape(-1,1)
+        f += (new_fluxes[:,inds[0]-1] - f[:,0]).reshape(-1, 1)
       new_fluxes[:,inds] = f
       
     # Save
